11c1393f62b2ed986a0aba28db09d7ffe8518958
jhs512 <jangka512@gmail.com>
2024-03-22 09:22:25 +0900

    work

---
+++ src/main/java/com/ll/sb20240322/Sb20240322Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20240322;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20240322Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20240322Application.class, args);
+    }
+
+}
